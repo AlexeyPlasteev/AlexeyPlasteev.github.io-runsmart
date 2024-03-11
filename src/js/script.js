8ef85deb1ebe453d3864b4e8a32225444978f2bf
Alexey <plasteev6@gmail.com>
2024-03-11 21:52:38 +0500

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -48,9 +48,61 @@ const slider = tns({
           $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
           $('.overlay, #order').fadeIn('slow');
         });
-      });
+      }); 
+      //form validation
+      function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
 
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    //number input mask
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
     });
-    })(jQuery);
+    //sending letters
+    $('form').submit (function(e) {
+      e.preventDefault();
+      if (!$(this).valid()) {
+        return;
+      }
+      $.ajax ({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize ()
+      }).done (function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+  })(jQuery);
+    
 
     
